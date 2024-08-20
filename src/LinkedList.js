d03171e9b5d395a01f5dba2ff1cb1d5db8e4dffa
Amanvir Singh <amanvir.singh880@gmail.com>
2024-08-19 23:56:02 -0400

    Added Pop Method

---
+++ src/LinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+          newTail = current;
+          current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+          this.head = null;
+          this.tail = null;
+        }
+        return current;
+      }
+}
