053a1ee0c021781e640abfbc731f6a662f9df1c2
Nandini Nischal <nischalnandini3@gmail.com>
2024-08-20 00:20:31 -0400

    Added Append Method

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -13,6 +13,19 @@ class LinkedList {
         this.length = 0;
     }
 
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+          this.head = newNode;
+          this.tail = newNode;
+        } else {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        }
+        this.length++;
+        return this;
+      }
+
     pop() {
         if (!this.head) return undefined;
         let current = this.head;
